db2b0954a549e92478e1e5523e6c14b8b1566da7
Pierre-François Pirlet <pfpirlet@hotmail.com>
2017-07-29 11:41:36 +0200

    working req.file

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const app = express();
 const dotenv = require("dotenv").config();
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'})
-const fs = require("fs");
 const port = process.env.PORT || 8080;
 
 app.get("/", function(req, res) {
 	res.sendFile("home.html", { root: __dirname });
 });
 
-app.post('/uploadFile', upload.single('uploadedFile'), function (req, res, next) {
-  var stats = fs.statSync(req.file); // à remplacer par la fonction size de multer
-  var size = stats["size"];
-  console.log(size);
+app.post('/upload', upload.single('uploadedFile'), function (req, res, next) {
+  console.log(req.file.size);
 })
 
 app.listen(port, function() {
